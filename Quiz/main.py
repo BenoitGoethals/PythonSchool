ddd6f8d02d2cc78e847bb9c5610da52f57e6d1ba
DESKTOP-UKCPG90\Benoit <benoit.goethals@gmail.com>
2024-08-09 07:31:47 +0200

    add

--- Quiz/main.py
+++ Quiz/main.py
@@ -3,7 +3,9 @@ from Quiz.model import QuestionMachine, QuestionPopulator
 
 
 def main():
-    qm=QuestionMachine(QuestionPopulator.construct_question(question_data))
+    qm = QuestionMachine(QuestionPopulator.construct_question(question_data))
+    qm.Start_question("")
+
 
 if __name__ == '__main__':
     main()
