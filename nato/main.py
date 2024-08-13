cc0be5b128e674d44bae4a99c78068c59e94530a
DESKTOP-UKCPG90\Benoit <benoit.goethals@gmail.com>
2024-08-13 19:45:39 +0200

    add

--- nato/main.py
+++ nato/main.py
@@ -13,9 +13,16 @@ def read_dataframe(param):
 def main():
     dat_dfa = read_dataframe("nato_phonetic_alphabet.csv")
     data = {value.letter: value.code for (index, value) in dat_dfa.iterrows()}
-    word = input("Enter a word: ")
-    nato_phonetic_alphabet = ' '.join([data.get(value.upper()) for value in word])
-    print(nato_phonetic_alphabet)
+    word = input("Enter a word: ").upper()
+    try:
+        nato_phonetic_alphabet = [data[value] for value in word]
+
+    except KeyError:
+        print("Sorry, that word is not in alphabet")
+    else:
+        data_convert = ' '.join(nato_phonetic_alphabet)
+        print(data_convert)
+
 
 if __name__ == '__main__':
     main()
